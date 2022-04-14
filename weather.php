e9b160eba96884c5e9100e306af1ba77a0b3f321
shivshankar2 <88234755+shivshankar2@users.noreply.github.com>
2022-04-14 09:27:22 +0530

    Add files via upload

---
+++ weather.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        body{
+            text-align: center;
+            font-size: 15px;
+            font-weight: bold;
+            font-style: italic;
+            background-color:darkseagreen;
+           
+        }
+        .weat{
+            font-size: 20px;
+            font-style: italic;
+
+        }
+    </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="weat">
+    <h1>Weather Information</h1>
+</div>
+    <h2> Make you own plan to visit North India</h2>
+    <div class="weather">
+    <tr>
+        <dt>October to February : Winter</dt>
+        <th>March to June : Summer</th><br>
+        <th>July to September: Monsoon</th><br>
+        <p></p>
+    </tr></div>
+    <h2>Make you own plan to Visit South India</h2>
+    <th>October to February : Winter</th><br>
+    <th>March to May : Summer</th><br>
+    <th>June to September : Monsoon</th><br>
+    <br>
+    <h2>Make your own plan to Visit East India</h2>
+    <th>October to February : Winter</th><br>
+    <th>March to May : Summer</th><br>
+    <th>June to September : Monsoon</th><br>
+    <br>
+    <h2>Make your own plan to Visit West India</h2>
+    <th>October to February : Winter</th><br>
+    <th>March to May : Summer</th><br>
+    <th>June to September : Monsoon</th><br>
+
+
+</body>
+</html>
