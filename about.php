7bd8d6d6d1ac8b89dbce814e76d28807f47139e7
shivshankar2 <88234755+shivshankar2@users.noreply.github.com>
2022-04-14 09:23:41 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+         body{
+             background-color: lightpink;
+             text-align: center;
+
+         }
+         .about1{
+             font-size: 30px;
+             height: 100%;
+             width: 100%;
+             background-image: url("taj2.jpg");
+            
+             
+
+             }
+             .about2{
+                 font-size: 30px;
+             }
+    </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>!OUR MISSION!</h1>
+    <div class="about1">
+    <p>To povide the detailed information About the Tourism in India.</p>
+    <br>
+    <p> This site is basically for Tourism World.
+        In this site we basically give the information of Famous,new tourists places in India.
+        So that you can easily visit the places.
+        <br>
+     </p></div>
+     <div class=" about2">
+
+      <p> more Information...
+          <br>
+
+          :
+          Contact:6205264243
+                : 7765919681
+                <br>
+        email   : shivshankarmahato2001@gmail.com
+
+        </p></div>
+
+</body>
+</html>
