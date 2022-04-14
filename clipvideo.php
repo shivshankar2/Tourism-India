2be39101a924c600155b2ef2faa169cf37c2d007
shivshankar2 <88234755+shivshankar2@users.noreply.github.com>
2022-04-14 09:25:36 +0530

    Add files via upload

---
+++ clipvideo.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+          body{
+              text-align: center;
+              background-color: skyblue;
+              background-image: url("bac.jfif");
+              font-style: italic;
+          }
+          #myVideo{
+              height: 50%;
+              width: 30%; 
+              float: left;
+              padding: 5px;
+              border: solid antiquewhite;
+               
+          }
+          .da1{
+            background-image: url("taj2.jpg");
+             
+          }
+            /* Three image containers (use 25% for four, and 50% for two, etc) */
+            
+    </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <div class="da1">
+    <h1>Beauty of Nature</h1>
+  </div>
+      <div class="container">
+        <div class="gallery">
+            <video autoplay muted loop id="myVideo">
+                <source src="Desert - 88660.mp4" type="video/mp4">
+              </video>
+              <div class="gallery">
+                <video autoplay muted loop id="myVideo">
+                    <source src="Traffic - 88921.mp4" type="video/mp4">
+                  </video>
+                  <div class="gallery">
+                    <video autoplay muted loop id="myVideo">
+                        <source src="Winter - 31318.mp4" type="video/mp4">
+                      </video>
+                      <div class="gallery">
+                        <video autoplay muted loop id="myVideo">
+                            <source src="Peacock - 77790.mp4" type="video/mp4">
+                          </video>
+                          <div class="gallery">
+                            <video autoplay muted loop id="myVideo">
+                                <source src="Flamingo - 49459.mp4" type="video/mp4">
+                              </video>
+                              <div class="gallery">
+                                <video autoplay muted loop id="myVideo">
+                                    <source src="Nature - 38678.mp4" type="video/mp4">
+                                  </video>
+                                  <div class="gallery">
+                                    <video autoplay muted loop id="myVideo">
+                                        <source src="Butterfly - 12060.mp4" type="video/mp4">
+                                      </video>
+                                      <div class="gallery">
+                                        <video autoplay muted loop id="myVideo">
+                                            <source src="Cloud - 2142.mp4" type="video/mp4">
+                                          </video>
+    
+
+        </div>
+         
+           
+</body>
+</html>
